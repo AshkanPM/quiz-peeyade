9bbca862d8e210972acbc737bdf1480a307e3120
Ashkan Pourghasem <ashkan.pourghasem>
2018-08-04 10:19:00 +0430

    Folder restructure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './assets/sass/base.scss';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
