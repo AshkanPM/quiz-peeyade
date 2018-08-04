fea70b0bd23e3bfb295c2fd9822d8a14f521bd8b
Ashkan Pourghasem <ashkan.pourghasem>
2018-08-04 10:08:17 +0430

    Enabled Sass modules in development

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.scss';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-
+            <div className={styles.App}>
+                <h3>You're all set!</h3>
             </div>
         );
     }
